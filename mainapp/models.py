68134cb8c334f9aa463295a2e150cec824c6b9cf
Myshadowss <109855548+Myshadowss@users.noreply.github.com>
2024-12-28 13:36:54 +0530

    model updated

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    """_summary_: This model is used to store all user details of the website """
+    phonenumber = models.CharField(max_length=20, unique=True, db_index=True)
+    updated_at = models.DateTimeField(_("Is updated"), auto_now=True)
+    role = models.CharField(
+        max_length=25,
+        choices=[("superadmin", "Super Admin"),("user", "User"), ("superuser", "Super User")],
+    )
+    # Adding custom related names to avoid conflict
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name=_("user groups"),
+        related_name='user_groups',  # Custom related name
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name=_("user permissions"),
+        related_name='user_permissions',  # Custom related name
+        blank=True
+    )
+    
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'phonenumber'
+    REQUIRED_FIELDS = ['username', 'email']
+
+    def __str__(self):
+        return f"{self.username} with role {self.role}"
+
+    class Meta:
+        verbose_name = _("User")
